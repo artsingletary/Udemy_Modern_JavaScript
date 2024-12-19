fe36ce99d2265b0f21c7560906baa1976792d3e0
Art Singletary <artsingletary@yahoo.com>
2024-12-19 06:16:20 -0500

    "strings2"

---
+++ Chapter 1/sandbox.js
@@ -0,0 +1,9 @@
+let email = 'mario@thenetninja.co.uk';
+
+//let result = email.lastIndexOf('n');
+
+//console.log(result);
+
+
+let result = email.slice(0,5);
+console.log(result);
---
+++ Chapter 1/sandbox1.js
@@ -0,0 +1,25 @@
+console.log('Hello World');
+
+// string concatenation
+let email = 'artsingletary@yahoo.com';
+let firstName = 'Art';
+let lastName = 'Singletary';
+
+let fullName = firstName + ' ' + lastName;
+
+
+console.log(fullName);
+
+// getting characters
+console.log(fullName[0]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result);
+
+let index = email.indexOf('@');
+console.log(index)
